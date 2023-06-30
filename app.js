e8b5f38d8f0896c3a260d4ad9e8c5c3566357735
Tomas Peters <tdpeters6794@gmail.com>
2023-06-30 16:50:33 -0400

    added functionality to change units

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+let searchedCity = {
+  name: "",
+  metric: false,
+};
+
 async function fetchWeather(city) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=ba6cb5364e724d34ab6154722232706&q=" ${city}`,
     { mode: "cors" }
   );
-  const weatherData = await response.json();
-  const newData = processData(weatherData);
-  console.log(newData);
-  displayData(newData);
+  let weatherData = await response.json();
+  let newData = processData(weatherData);
+
+  if (searchedCity.metric == false) {
+    displayAmericanData(newData);
+  } else {
+    displayMetricData(newData);
+  }
 }
 
 function processData(weatherData) {
-  const newData = {
+  let newData = {
     condition: weatherData.current.condition.text,
     icon: weatherData.current.condition.icon,
     iconCode: weatherData.current.condition.code,
@@ -34,7 +43,7 @@ function processData(weatherData) {
   return newData;
 }
 
-function displayData(newData) {
+function displayAmericanData(newData) {
   document.querySelector(".city").innerText = "Weather in " + newData.city;
   document.querySelector(".temp").innerText = newData.currentTemp.f + "°F";
   document.querySelector(".icon").src = newData.icon;
@@ -46,39 +55,47 @@ function displayData(newData) {
     "Wind: " + newData.wind.mph.speed + "mph " + newData.wind.mph.direction;
 
   document.querySelector(".weather").classList.remove("loading");
-  document
-    .querySelector(".change-button")
-    .addEventListener("click", function () {
-      if (document.querySelector(".temp").innerText.includes("F")) {
-        document.querySelector(".temp").innerText =
-          newData.currentTemp.c + "°C";
-        document.querySelector(".wind").innerText =
-          "Wind: " +
-          newData.wind.kph.speed +
-          "kph " +
-          newData.wind.kph.direction;
-      } else if (document.querySelector(".temp").innerText.includes("C")) {
-        document.querySelector(".temp").innerText =
-          newData.currentTemp.f + "°F";
-        document.querySelector(".wind").innerText =
-          "Wind: " +
-          newData.wind.mph.speed +
-          "mph " +
-          newData.wind.mph.direction;
-      }
-    });
+}
+
+function displayMetricData(newData) {
+  document.querySelector(".city").innerText = "Weather in " + newData.city;
+  document.querySelector(".temp").innerText = newData.currentTemp.c + "°C";
+  document.querySelector(".icon").src = newData.icon;
+  document.body.style.backgroundImage = `url(${newData.icon})`;
+  document.querySelector(".description").innerText = newData.condition;
+  document.querySelector(".humidity").innerText =
+    "Humidity: " + newData.humidity + "%";
+  document.querySelector(".wind").innerText =
+    "Wind: " + newData.wind.kph.speed + "kph " + newData.wind.kph.direction;
+
+  document.querySelector(".weather").classList.remove("loading");
 }
 
 document.querySelector(".search button").addEventListener("click", function () {
-  fetchWeather(document.querySelector(".search-bar").value);
+  searchedCity.name = document.querySelector(".search-bar").value;
   document.querySelector(".search-bar").value = "";
+  fetchWeather(searchedCity.name);
 });
 
 document
   .querySelector(".search-bar")
   .addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-      fetchWeather(document.querySelector(".search-bar").value);
+      searchedCity.name = document.querySelector(".search-bar").value;
       document.querySelector(".search-bar").value = "";
+      fetchWeather(searchedCity.name);
     }
   });
+
+document.querySelector(".change-button").addEventListener("click", function () {
+  if (searchedCity.metric == false) {
+    searchedCity.metric = true;
+    fetchWeather(searchedCity.name);
+  } else if (searchedCity.metric == true) {
+    searchedCity.metric = false;
+    fetchWeather(searchedCity.name);
+  }
+  console.log(searchedCity);
+});
+
+// fetchWeather("Richmond");
